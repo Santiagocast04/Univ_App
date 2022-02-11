0821e81e374de933e202edc2a193b1454c57a9aa
Fabio Andrés Castañeda <fcastaneda@picap.co>
2022-02-11 18:47:25 -0500

    add cards and styling on the page

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+gem 'jquery-rails'
 gem 'material_icons'
 gem 'materialize-sass', '~> 1.0.0'
 # Use Redis adapter to run Action Cable in production
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,13 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require turbolinks
 //= require materialize
 //= require_tree .
+
+$( document ).on('turbolinks:load',function() {
+    $(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
+})
