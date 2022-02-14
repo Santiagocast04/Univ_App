f5183e48ec5560d223b9d97310c893d562acb9a7
Fabio Andrés Castañeda <fcastaneda@picap.co>
2022-02-14 09:55:26 -0500

    implement Log in functionality

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,10 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
   def new
@@ -15,7 +15,7 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
     if @student.save
       flash[:notice] = "You have successfully signed up"
-      redirect_to root_path
+      redirect_to @student
     else
       render 'new'
     end
@@ -37,7 +37,12 @@ class StudentsController < ApplicationController
 
   private
 
+  def set_student
+    @student = Student.find(params[:id])
+
+  end
+
   def student_params
-    params.require(:student).permit(:name, :email)
+    params.require(:student).permit(:name, :email, :password, :password_confirmation)
   end
 end
